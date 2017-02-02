f1a8f332e46735a1b5e64c142afbca01c3a1f43e
Mikael Johansson <mikael@narrativeteam.com>
2017-02-02 14:42:01 +0100

    Multiplikationstabell

--- jstest.js
+++ jstest.js
@@ -4,7 +4,9 @@
 /* Globala variabler */
 var counter = 0
 
+/***********************/
 /*** Hjälpfunktioner ***/
+/***********************/
 var skriv = function(htmlText) {
 	document.getElementById('testdiv').innerHTML += htmlText;
 }
@@ -14,10 +16,13 @@ var testRensa = function() {
 	document.getElementById('testdiv').innerHTML = '';
 }
 
+/*******************************/
 /*** Funktioner att leka med ***/
+/*******************************/
 var skrivHej = function() {
 	skriv('<font color="#f00">HEJ!</font> <br/>')
 }
+
 
 var skrivNummer = function() {
 	antal_loopar = 5
@@ -50,13 +55,16 @@ var skrivNummer = function() {
 	}
 }
 
+
 /*** Den här funktionen anropas när du trycker på "Kör" ***/
 var testFunktion = function() {
 	console.log('Hej konsollen!')
 
 	skrivHej();
 	skrivNummer();
+	//multiplikationsTabell();
 }
+
 
 var addComment = function() {
 	namn = document.getElementById('commentName').value
@@ -68,3 +76,37 @@ var addComment = function() {
 	document.getElementById('commentName').value = ''
 	document.getElementById('commentComment').value = ''
 }
+
+
+var multiplikationsTabell = function() {
+	width = parseInt(document.getElementById('multWidth').value)
+	height = parseInt(document.getElementById('multHeight').value)
+
+	str = ''
+	str += '<h3>Multiplikationstabell ' + width + 'x' + height + '</h3>'
+	str += '<table border="1px">'
+	
+	for (y = 1; y < height+1; y++) {
+		str += '<tr>'
+		for (x = 1; x < width+1; x++) {
+			var produkt = x * y
+
+			/* Beräkna om vi ska ha en speciell färg */
+			if (x == y) {
+				klass = 'kvadratcell'
+			} else if (x == 1 || y == 1) {
+				klass = 'kantcell'
+			} else {
+				klass = 'standardcell'
+			}
+
+			str += '<td class="' + klass + '">'
+			str += produkt
+			str += '</td>'
+		}
+		str += '</tr>'
+	}
+	str += '</table>'
+	skriv(str)
+}
+
